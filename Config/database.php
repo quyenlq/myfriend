643e955a509f2a7d6d979b7c480c8b69008d8153
Luc Quoc Quyen <lucquocquyen@gmail.com>
2013-05-07 22:24:19 +0700

    init commit

---
+++ Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $myfriend = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'port' => 3306,
+		'login' => 'root',
+		'password' => '',
+		'database' => 'myfriend',
+	);
+}
